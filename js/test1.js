c7751bf458ee9d8714fc0e96a099782cf50c734e
chaoxunzhou <mikezhouxun@foxmail.com>
2018-04-23 17:54:45 -0400

    first commit

---
+++ js/test1.js
@@ -0,0 +1,46 @@
+var model =  {
+
+	gamesname :
+	[	
+		{	
+			companyName : 'Big Viking Games',
+			yoWorldName : 'Yo World',
+			fishWorldName : 'Fish World',
+			calatronName : 'calatron'
+			tinyTappersName : 'Tiny Tappers'
+		},
+		{	
+			companyName : 'Big Blue Bubble',
+			mySingingMonsterSeriesName : 'My Singing Monster Series',
+			JungleGuardiansName : 'Jungle Guardians',
+			WreckingSquardName : 'Wrecking Squard',
+			JammerSplashName : 'Jammer Splash',
+			ZombieBloxxName : 'Zombie Bloxx',		
+			ShortFusedName : 'Short Fused',
+			HomeArcadeName : 'Home Arcade',
+			PixelDogersName : 'Pixel Dogers'
+		},
+		{
+			companyName: 'Digita Lextremes',
+			Warframename : 'Warframe'
+		},
+		{
+			companyName : 'Tactie Studio',
+			ImmortalEmpirename : 'Immortal Empire'
+		},
+		{
+			companyName : 'Tiny Titan Studios' ,
+			dashQuestHerosName : 'Dash Quest Heros',
+			tinyRailsame : 'Tiny Rails',
+			dashQuestName : 'Dash Quest',
+			TapSmithsName : 'Tap Smiths',
+			happyCubeName : 'Happy Cube',
+			starshipTurrentOperatorGuyName : 'Starship Turrent Operator Guy'
+	];
+};
+
+var views = {
+	
+
+
+}
---
+++ js/test2.js
@@ -0,0 +1,134 @@
+var model = {
+ourGames : [
+			{
+				id = 0,
+				companyName : 'Big Viking Games',
+				gamesName : 'Yo World',
+				logoImg : ''
+			},
+			{
+				id = 1,
+				companyName : 'Big Viking Games',
+				gamesName : 'Fish World',
+				logoImg : ''
+			},
+			{
+				companyName : 'Big Viking Games',
+				gamesName : 'Calatron',
+				logoImg : ''
+			},
+			{
+				companyName : 'Big Viking Games',
+				gamesName : 'Tiny Tappers'
+				logoImg : ''
+			},
+			{
+				companyName : 'Big Blue Bubble',
+				gamesName :'My Singing Monster Series',
+				logoImg : ''
+			},
+			{
+				companyName : 'Big Blue Bubble',
+				gamesName :'Jungle Guardians',
+				logoImg : ''
+			},
+			{
+				companyName : 'Big Blue Bubble',
+				gamesName :'Wrecking Squard',
+				logoImg : ''
+			},
+			{
+				companyName : 'Big Blue Bubble',
+				gamesName :'Jammer Splash',
+				logoImg : ''
+			},
+			{
+				companyName : 'Big Blue Bubble',
+				gamesName :'Zombie Bloxx',	
+				logoImg : ''
+			},
+			{
+				companyName : 'Big Blue Bubble',
+				gamesName :'Short Fused',
+				logoImg : ''
+			},
+			{
+				companyName : 'Big Blue Bubble',
+				gamesName :'Short Fused',
+				logoImg : ''
+			},
+			{
+				companyName : 'Big Blue Bubble',
+				gamesName :'Home Arcade',
+				logoImg : ''
+			},
+			{
+				companyName : 'Big Blue Bubble',
+				gamesName :'Pixel Dogers'
+				logoImg : ''
+			},
+			{
+				companyName: 'Digita Lextremes',
+				gamesName : 'Warframe'
+				logoImg : ''
+			},
+			{
+				companyName: 'Tactie Studio',
+				gamesName : 'Immortal Empire'
+				logoImg : ''
+			},
+			{
+				companyName: 'Tiny Titan Studios' ,
+				gamesName : 'Dash Quest Heros',
+				logoImg : ''
+			},
+			{
+				companyName: 'Tiny Titan Studios' ,
+				gamesName : 'Dash Quest ',
+				logoImg : ''
+			},
+			{
+				companyName: 'Tiny Titan Studios' ,
+				gamesName : 'Tiny Rails',
+				logoImg : ''
+			},
+			{
+				companyName: 'Tiny Titan Studios' ,
+				gamesName : 'Tap Smiths',
+				logoImg : ''
+			},
+			{
+				companyName: 'Tiny Titan Studios' ,
+				gamesName : 'Happy Cube',
+				logoImg : ''
+			},
+			{
+				companyName: 'Tiny Titan Studios' ,
+				gamesName : 'Starship Turrent Operator Guy',
+				logoImg : ''
+			}
+
+		]
+
+		company : [
+			'Big Viking Games','Big Blue Bubble','Digita Lextremes','Tactie Studio','Tiny Titan Studios' ,
+
+		],
+
+		bigVikinggGames : [
+			'Yo World','Fish World','calatron','Tiny Tappers'
+		],
+		bigBlueBubble : [
+			'My Singing Monster Series','Jungle Guardians','Wrecking Squard','Jammer Splash','Zombie Bloxx','Short Fused','Home Arcade','Pixel Dogers'	
+		],
+		digitaLextremes : [
+			'Warframe'
+		],
+		tactieStudio : [
+			'Immortal Empire'
+		],
+		tinyTitanStudios: [
+			'Dash Quest Heros','Tiny Rails','Dash Quest','Tap Smiths','Happy Cube','Starship Turrent Operator Guy'
+		],
+
+}
